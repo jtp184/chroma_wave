c975dd457edd3d1c24c58bccb8178e1b57f129e2
Justin Piotroski <justin.piotroski@gmail.com>
2026-03-03 12:02:02 -0800

    feat: Add Barcode.measure for layout planning

--- lib/chroma_wave/barcode.rb
+++ lib/chroma_wave/barcode.rb
@@ -32,6 +32,36 @@ module ChromaWave
     #   @return [Integer] number of modules in the binary encoding
     Metrics = Data.define(:width, :height, :encoding_length)
 
+    # Computes the pixel dimensions of a barcode without rendering it.
+    #
+    # @param data [String] the data to encode
+    # @param symbology [Symbol] symbology key (e.g. +:code128+, +:ean13+)
+    # @param module_width [Integer] pixel width per narrow bar
+    # @param height [Integer] bar height in pixels
+    # @param include_text [Boolean] whether text will be rendered below bars
+    # @param text_font [Font, nil] font for text (required if include_text)
+    # @return [Metrics] frozen value object with width, height, and encoding length
+    # @raise [ArgumentError] if symbology is unknown, data is invalid, or text_font missing
+    # @raise [DependencyError] if barby is not installed
+    def self.measure(data, symbology:, module_width: 2, height: 60, include_text: false, text_font: nil)
+      if include_text && text_font.nil?
+        raise ArgumentError,
+              'text_font: is required when include_text: true'
+      end
+
+      barcode = build_barcode(symbology, data)
+      encoding = barcode.encoding.is_a?(Array) ? barcode.encoding.join : barcode.encoding
+
+      total_width = encoding.length * module_width
+      total_height = if include_text
+                       height + 2 + (text_font.line_height * 1.2).round
+                     else
+                       height
+                     end
+
+      Metrics.new(width: total_width, height: total_height, encoding_length: encoding.length)
+    end
+
     # Builds a Barby barcode instance for the given symbology and data.
     #
     # @param symbology [Symbol] symbology key (e.g. +:code128+, +:ean13+)
